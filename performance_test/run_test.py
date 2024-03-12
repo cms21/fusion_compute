9e1edbb5a3967ff3f6315e48d3b820ddaca06509
Christine Simpson <christine.simpson@gmail.com>
2024-03-12 01:10:45 -0500

    test runner

---
+++ performance_test/run_test.py
@@ -0,0 +1,9 @@
+from performance_test.performance_test import make_sequential_test
+from performance_test.performance_test import assemble_ionorb_input_kwargs
+
+if __name__ == '__main__':
+    inputs = assemble_ionorb_input_kwargs(testfile="collection_of_multi-slice_shots.txt",
+                                          nparts=[50000])
+    #inputs = assemble_ionorb_input_kwargs(nparts=[1000,10000])
+
+    make_sequential_test(inputs, machines=["polaris"],niter=1)
